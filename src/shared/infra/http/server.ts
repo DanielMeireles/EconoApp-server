48a38d2891b2ffeaefd37c88265e03b4ebe0c2f8
DanielMeireles <daniel.meireles@outlook.com>
2020-09-07 13:36:39 -0300

    Adjust on server.ts for use routes

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,18 +1,45 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import ora from 'ora';
 import { errors } from 'celebrate';
 import 'express-async-errors';
 
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import rateLimiter from '@shared/infra/http/middlewares/RateLimiter';
+import routes from './routes';
+
+import '@shared/infra/typeorm';
+import '@shared/container';
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.uploadsFolder));
+app.use(rateLimiter);
+app.use(routes);
 
 app.use(errors());
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 const port = Number(process.env.APP_PORT);
 
