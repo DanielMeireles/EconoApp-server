1aca3491b852adf0feb193a0ffb05757ad0d2fd1
DanielMeireles <daniel.meireles@outlook.com>
2020-09-07 10:58:00 -0300

    Add config of upload files

---
+++ src/config/upload.ts
@@ -0,0 +1,47 @@
+import path from 'path';
+import crypto from 'crypto';
+import multer from 'multer';
+
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: multer.StorageEngine;
+  };
+  config: {
+    disk: unknown;
+    aws: {
+      bucket?: string;
+      region?: string;
+    };
+  };
+}
+
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+const uploadsFolder = path.resolve(tmpFolder, 'uploads');
+
+const UploadConfig: IUploadConfig = {
+  driver: process.env.STORAGE_DRIVER as 's3' | 'disk',
+  tmpFolder,
+  uploadsFolder,
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename(request, file, callback) {
+        const fileHash = crypto.randomBytes(10).toString('hex');
+        const filename = `${fileHash}-${file.originalname}`;
+        return callback(null, filename);
+      },
+    }),
+  },
+  config: {
+    disk: {},
+    aws: {
+      bucket: process.env.AWS_BUCKET,
+      region: process.env.AWS_REGION,
+    },
+  },
+};
+
+export default UploadConfig;
