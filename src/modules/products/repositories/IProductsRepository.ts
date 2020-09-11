186ef3f25435267a018c4e66613c02f82a180e76
DanielMeireles <daniel.meireles@outlook.com>
2020-09-11 19:28:18 -0300

    Add interface for ProductRepository

---
+++ src/modules/products/repositories/IProductsRepository.ts
@@ -0,0 +1,11 @@
+import Product from '@modules/products/infra/typeorm/entities/Product';
+import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
+
+interface IProductsRepository {
+  findByName(name: string): Promise<Product | undefined>;
+  findByDescription(description: string): Promise<Product | undefined>;
+  create(data: ICreateProductDTO): Promise<Product>;
+  save(product: Product): Promise<Product>;
+}
+
+export default IProductsRepository;
