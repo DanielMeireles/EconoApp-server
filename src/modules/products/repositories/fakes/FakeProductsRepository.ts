09143430491ddc18438663c33ca7f67c9b648e05
DanielMeireles <daniel.meireles@outlook.com>
2020-09-11 19:40:27 -0300

    Add fake repository of Products to use in tests with jest

---
+++ src/modules/products/repositories/fakes/FakeProductsRepository.ts
@@ -0,0 +1,51 @@
+import { uuid } from 'uuidv4';
+import IProductsRepository from '@modules/products/repositories/IProductsRepository';
+
+import ICreateProductDTO from '@modules/products/dtos/ICreateProductDTO';
+
+import Product from '@modules/products/infra/typeorm/entities/Product';
+
+class FakeProductsRepository implements IProductsRepository {
+  private products: Product[] = [];
+
+  public async findByName(name: string): Promise<Product | undefined> {
+    const findProduct = this.products.find(product => product.name === name);
+
+    return findProduct;
+  }
+
+  public async findByDescription(
+    description: string,
+  ): Promise<Product | undefined> {
+    const findProduct = this.products.find(
+      product => product.description === description,
+    );
+
+    return findProduct;
+  }
+
+  public async create({
+    name,
+    description,
+  }: ICreateProductDTO): Promise<Product> {
+    const product = new Product();
+
+    Object.assign(product, { id: uuid(), name, description });
+
+    this.products.push(product);
+
+    return product;
+  }
+
+  public async save(product: Product): Promise<Product> {
+    const findIndex = this.products.findIndex(
+      findProduct => findProduct.id === product.id,
+    );
+
+    this.products[findIndex] = product;
+
+    return product;
+  }
+}
+
+export default FakeProductsRepository;
