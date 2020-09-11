bfda0ea7eb1da1c95a27a907e9ad599db452d22a
DanielMeireles <daniel.meireles@outlook.com>
2020-09-11 20:15:06 -0300

    Add test of create user service

---
+++ src/modules/products/services/CreateProductService.spec.ts
@@ -0,0 +1,43 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeProductsRepository from '@modules/products/repositories/fakes/FakeProductsRepository';
+import FakeCacheProvider from '@shared/container/providers/CacheProvider/fakes/FakeCacheProvider';
+import CreateProductService from '@modules/products/services/CreateProductService';
+
+let fakeProductsRepository: FakeProductsRepository;
+let createProduct: CreateProductService;
+let fakeCacheProvider: FakeCacheProvider;
+
+describe('CreateProduct', () => {
+  beforeEach(() => {
+    fakeProductsRepository = new FakeProductsRepository();
+    fakeCacheProvider = new FakeCacheProvider();
+    createProduct = new CreateProductService(
+      fakeProductsRepository,
+      fakeCacheProvider,
+    );
+  });
+
+  it('should be able to create a new product', async () => {
+    const product = await createProduct.execute({
+      name: 'Product 1',
+      description: 'Product 1',
+    });
+
+    expect(product).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new product with same name from another', async () => {
+    await createProduct.execute({
+      name: 'Product 1',
+      description: 'Product 1',
+    });
+
+    await expect(
+      createProduct.execute({
+        name: 'Product 1',
+        description: 'Product 1',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
