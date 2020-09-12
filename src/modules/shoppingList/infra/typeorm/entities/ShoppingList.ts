146527d2a21c30dc045600115fad26ad2c5c133c
DanielMeireles <daniel.meireles@outlook.com>
2020-09-12 12:50:03 -0300

    Add entity of Shopping List

---
+++ src/modules/shoppingList/infra/typeorm/entities/ShoppingList.ts
@@ -0,0 +1,62 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { Expose } from 'class-transformer';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+import uploadConfig from '@config/upload';
+
+@Entity('shoppinglist')
+class ShoppingList {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column('timestamp with time zone')
+  date: Date;
+
+  @Column()
+  image: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Expose({ name: 'image_url' })
+  getImageUrl(): string | null {
+    if (!this.image) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.image}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.image}`;
+      default:
+        return null;
+    }
+  }
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default ShoppingList;
