20bc47e8331fb8510608d7934900fc486e42785b
DanielMeireles <daniel.meireles@outlook.com>
2020-09-12 15:24:36 -0300

    Add dto of find shopping lists by user id and adjust on interface of shopping list repository

---
+++ src/modules/shoppingLists/dtos/IFindShoppingListsByUserIdDTO.ts
@@ -0,0 +1,5 @@
+interface IFindShoppingListsByUserIdDTO {
+  user_id: string;
+}
+
+export default IFindShoppingListsByUserIdDTO;
--- src/modules/shoppingLists/repositories/IShoppingListsRepository.ts
+++ src/modules/shoppingLists/repositories/IShoppingListsRepository.ts
@@ -1,15 +1,19 @@
 import ShoppingList from '@modules/shoppingLists/infra/typeorm/entities/ShoppingList';
 import ICreateShoppingListDTO from '@modules/shoppingLists/dtos/ICreateShoppingListDTO';
+import IFindShoppingListsByIdDTO from '@modules/shoppingLists/dtos/IFindShoppingListsByIdDTO';
+import IFindShoppingListsByNameDTO from '@modules/shoppingLists/dtos/IFindShoppingListsByNameDTO';
+import IFindShoppingListsByDescriptionDTO from '@modules/shoppingLists/dtos/IFindShoppingListsByDescriptionDTO';
+import IFindShoppingListsByDateDTO from '@modules/shoppingLists/dtos/IFindShoppingListsByDateDTO';
+import IFindShoppingListsByUserIdDTO from '@modules/shoppingLists/dtos/IFindShoppingListsByUserIdDTO';
 
 interface IShoppingListsRepository {
-  findAll(user_id: string): Promise<ShoppingList[]>;
-  findById(user_id: string, id: string): Promise<ShoppingList | undefined>;
-  findByName(user_id: string, name: string): Promise<ShoppingList[]>;
+  findAll(data: IFindShoppingListsByUserIdDTO): Promise<ShoppingList[]>;
+  findById(data: IFindShoppingListsByIdDTO): Promise<ShoppingList | undefined>;
+  findByName(data: IFindShoppingListsByNameDTO): Promise<ShoppingList[]>;
   findByDescription(
-    user_id: string,
-    description: string,
+    data: IFindShoppingListsByDescriptionDTO,
   ): Promise<ShoppingList[]>;
-  findByDate(user_id: string, date: Date): Promise<ShoppingList[]>;
+  findByDate(data: IFindShoppingListsByDateDTO): Promise<ShoppingList[]>;
   create(data: ICreateShoppingListDTO): Promise<ShoppingList>;
   save(shoppingList: ShoppingList): Promise<ShoppingList>;
 }
