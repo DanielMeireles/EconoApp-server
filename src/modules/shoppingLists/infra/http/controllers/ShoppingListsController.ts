82fe1be3db345468ed8eded1d6f147f3cfc016f2
DanielMeireles <daniel.meireles@outlook.com>
2020-09-12 21:49:15 -0300

    Add shopping lists controller

---
+++ src/modules/shoppingLists/infra/http/controllers/ShoppingListsController.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { classToClass, plainToClass } from 'class-transformer';
+
+import CreateShoppingListService from '@modules/shoppingLists/services/CreateShoppingListService';
+import ListShoppingListsService from '@modules/shoppingLists/services/ListShoppingListsService';
+import ShoppingList from '@modules/shoppingLists/infra/typeorm/entities/ShoppingList';
+
+class ProductsController {
+  public async create(req: Request, res: Response): Promise<Response> {
+    const { name, description, date, user_id } = req.body;
+
+    const createShoppingList = container.resolve(CreateShoppingListService);
+
+    const shoppingList = await createShoppingList.execute({
+      name,
+      description,
+      date,
+      user_id,
+    });
+
+    return res.json(shoppingList);
+  }
+
+  public async index(req: Request, res: Response): Promise<Response> {
+    const user_id = req.user.id;
+
+    const listShoppingLists = container.resolve(ListShoppingListsService);
+
+    const shoppingLists = await listShoppingLists.execute({ user_id });
+
+    return res.json(classToClass(plainToClass(ShoppingList, shoppingLists)));
+  }
+}
+
+export default ProductsController;
