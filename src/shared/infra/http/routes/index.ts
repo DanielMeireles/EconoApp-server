bc9d725ec83edf975be4aba49059dc48d1e3008d
DanielMeireles <daniel.meireles@outlook.com>
2020-09-12 22:20:14 -0300

    Add shopping lists routes in file of routes

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -5,6 +5,7 @@ import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordsRouter from '@modules/users/infra/http/routes/passwords.routes';
 import profilesRouter from '@modules/users/infra/http/routes/profiles.routes';
 import productsRouter from '@modules/products/infra/http/routes/products.routes';
+import shoppingListsRouter from '@modules/shoppingLists/infra/http/routes/shoppingLists.routes';
 
 const routes = Router();
 
@@ -13,5 +14,6 @@ routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordsRouter);
 routes.use('/profile', profilesRouter);
 routes.use('/products', productsRouter);
+routes.use('/shoppinglists', shoppingListsRouter);
 
 export default routes;
