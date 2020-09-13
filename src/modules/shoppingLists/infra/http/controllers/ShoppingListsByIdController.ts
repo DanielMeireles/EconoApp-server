0dd0e31280b44187e037f5e91c6d24bce1f369a9
DanielMeireles <daniel.meireles@outlook.com>
2020-09-12 21:52:23 -0300

    Add shopping lists by id controller

---
+++ src/modules/shoppingLists/infra/http/controllers/ShoppingListsByIdController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { classToClass, plainToClass } from 'class-transformer';
+
+import ListShoppingListsByIdService from '@modules/shoppingLists/services/ListShoppingListsByIdService';
+import ShoppingList from '@modules/shoppingLists/infra/typeorm/entities/ShoppingList';
+
+class ShoppingListsByIdController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const user_id = req.user.id;
+    const { id } = req.params;
+    const listShoppingLists = container.resolve(ListShoppingListsByIdService);
+    const shoppingLists = await listShoppingLists.execute({ user_id, id });
+
+    return res.json(classToClass(plainToClass(ShoppingList, shoppingLists)));
+  }
+}
+
+export default ShoppingListsByIdController;
