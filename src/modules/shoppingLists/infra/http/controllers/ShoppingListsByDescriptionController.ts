f223fc39533e9002d528861661bc653fa0d6d435
DanielMeireles <daniel.meireles@outlook.com>
2020-09-12 21:57:54 -0300

    Add shopping lists by description controller

---
+++ src/modules/shoppingLists/infra/http/controllers/ShoppingListsByDescriptionController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import { classToClass, plainToClass } from 'class-transformer';
+
+import ListShoppingListsByDescriptionService from '@modules/shoppingLists/services/ListShoppingListsByDescriptionService';
+import ShoppingList from '@modules/shoppingLists/infra/typeorm/entities/ShoppingList';
+
+class ShoppingListsByDescriptionController {
+  public async index(req: Request, res: Response): Promise<Response> {
+    const user_id = req.user.id;
+    const { description } = req.params;
+    const listShoppingLists = container.resolve(
+      ListShoppingListsByDescriptionService,
+    );
+    const shoppingLists = await listShoppingLists.execute({
+      user_id,
+      description,
+    });
+
+    return res.json(classToClass(plainToClass(ShoppingList, shoppingLists)));
+  }
+}
+
+export default ShoppingListsByDescriptionController;
